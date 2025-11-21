ec357bc1df15177d02cc9aa14e9abeb191e3a00a
Yohan Giarelli <yohan@les-tilleuls.coop>
2025-11-21 12:36:10 +0100

    fix: Fixed mutation score (#13)

--- tests/Extension/Symfony/Bundle/SorterBundleTest.php
+++ tests/Extension/Symfony/Bundle/SorterBundleTest.php
@@ -6,8 +6,11 @@ namespace Sorter\Tests\Extension\Symfony\Bundle;
 
 use Sorter\Builder\QueryParamUrlBuilder;
 use Sorter\Builder\UrlBuilder;
+use Sorter\Extension\Symfony\Bundle\DependencyInjection\Compiler\ApplierCompilerPass;
+use Sorter\Extension\Symfony\Bundle\SorterBundle;
 use Sorter\SorterFactory;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 
 final class SorterBundleTest extends KernelTestCase
 {
@@ -19,5 +22,16 @@ final class SorterBundleTest extends KernelTestCase
         $this->assertInstanceOf(QueryParamUrlBuilder::class, $kernel->getContainer()->get(UrlBuilder::class));
         $this->assertTrue($kernel->getContainer()->has(SorterFactory::class));
         $this->assertTrue($kernel->getContainer()->has('sorter.factory'));
+    }
+
+    public function testCompilerPassIsAdded(): void
+    {
+        $containerMock = $this->createMock(ContainerBuilder::class);
+        $containerMock->expects($this->once())
+            ->method('addCompilerPass')
+            ->with($this->isInstanceOf(ApplierCompilerPass::class));
+
+        $bundle = new SorterBundle();
+        $bundle->build($containerMock);
     }
 }
